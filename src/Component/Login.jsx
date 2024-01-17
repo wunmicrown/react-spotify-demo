9de1145c93e2d6d97c3767eb30f16833d10765b6
Wunmicrown <olayiwolapamilerin34@gmail.com>
2024-01-17 22:03:53 +0100

    committed

--- src/Component/Login.jsx
+++ src/Component/Login.jsx
@@ -1,72 +1,106 @@
-
-
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const Login = () => {
-    const [name, setName] = useState("SQI template")
-    const [allUsers, setAllUsers] = useState([]);
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [name, setName] = useState("SQI template");
+  const [allUsers, setAllUsers] = useState([]);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const user = (i) =>{
-      setFirstName(i.target.value)
-    }
-    const user1 = (i) =>{
-      setLastName(i.target.value)
-    }
-    const user2 = (i) =>{
-      setEmail(i.target.value)
-    }
-    const user3 = (i) =>{
-      setPassword(i.target.value)
-    }
+  const user = (i) => {
+    setFirstName(i.target.value);
+  };
+  const user1 = (i) => {
+    setLastName(i.target.value);
+  };
+  const user2 = (i) => {
+    setEmail(i.target.value);
+  };
+  const user3 = (i) => {
+    setPassword(i.target.value);
+  };
 
-   
+  const changeName = () => {
+    const newUser = {
+      firstName,
+      lastName,
+      email,
+      password,
+    };
 
-    const changeName = () =>{
-      const newUser = {
-        firstName,
-        lastName,
-        email,
-        password,
-      };
-  
-      setAllUsers((prevUsers) => [...prevUsers, newUser]);
-      setFirstName('');
-      setLastName('');
-      setEmail('');
-      setPassword('');
+    setAllUsers((prevUsers) => [...prevUsers, newUser]);
+    setFirstName('');
+    setLastName('');
+    setEmail('');
+    setPassword('');
+  };
 
-      // console.log(allUsers);
-    }
-    
   return (
-    <div>
-      <h2>
-        {name}
-      </h2>
-        <h1>
-        {allUsers.map((user, index) => (
-          <tr key={index}>
-            <td>{index+1}</td>
-            <td>{user.firstName}</td>
-            <td>{user.lastName}</td>
-            <td>{user.email}</td>
-            <td>{user.email}</td>
-               
+    <div className="container mx-auto p-4">
+      <h2 className="text-2xl font-semibold mb-4">{name}</h2>
+      
+      <table className="table-auto w-full mb-8">
+        <thead>
+          <tr>
+            <th className="px-4 py-2">ID</th>
+            <th className="px-4 py-2">First Name</th>
+            <th className="px-4 py-2">Last Name</th>
+            <th className="px-4 py-2">Email</th>
+            <th className="px-4 py-2">Password</th>
           </tr>
-        ))}
-        </h1>
+        </thead>
+        <tbody>
+          {allUsers.map((user, index) => (
+            <tr key={index} className={(index % 2 === 0) ? 'bg-gray-100' : 'bg-white'}>
+              <td className="border px-4 py-2">{index + 1}</td>
+              <td className="border px-4 py-2">{user.firstName}</td>
+              <td className="border px-4 py-2">{user.lastName}</td>
+              <td className="border px-4 py-2">{user.email}</td>
+              <td className="border px-4 py-2">{user.password}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
-        <input type="text" onChange={user} placeholder='first name' value={firstName} />
-        <input type="text" onChange={user1} placeholder='last name' value={lastName} />
-        <input type="text" onChange={user2} placeholder='last name'  value={email} />
-        <input type="text" onChange={user3} placeholder='last name' value={password}/>
-        <button onClick={changeName}>change name</button>
+      <div className="flex space-x-2 mb-4">
+        <input
+          type="text"
+          onChange={user}
+          placeholder="First Name"
+          value={firstName}
+          className="border px-4 py-2 w-1/4"
+        />
+        <input
+          type="text"
+          onChange={user1}
+          placeholder="Last Name"
+          value={lastName}
+          className="border px-4 py-2 w-1/4"
+        />
+        <input
+          type="text"
+          onChange={user2}
+          placeholder="Email"
+          value={email}
+          className="border px-4 py-2 w-1/4"
+        />
+        <input
+          type="text"
+          onChange={user3}
+          placeholder="Password"
+          value={password}
+          className="border px-4 py-2 w-1/4"
+        />
+        <button
+          onClick={changeName}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Change Name
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
